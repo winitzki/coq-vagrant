a544c89724c716062dd430c2d1f677ea174810e6
Sergei Winitzki <sergei@versal.com>
2016-05-06 20:03:55 -0700

    now includes GUI

--- init_as_root.sh
+++ init_as_root.sh
@@ -2,6 +2,8 @@
 
 install="apt-get install --force-yes --yes"
 
+vagrantuser="$vagrant"
+
 function check_md5 {
   local file="$1" md5="$2"
   [ `md5sum < "$file" | cut -c 1-32` == "$md5" ]
@@ -28,7 +30,7 @@ function install_if_needed {
   fi
 }
 
-for p in emacs coq coqide proofgeneral proofgeneral-doc proofgeneral-coq; do
+for p in emacs coq coqide proofgeneral proofgeneral-doc proofgeneral-coq nodm xserver-xorg xinit openbox xterm mupdf; do
   install_if_needed $p
 done
 
@@ -42,3 +44,20 @@ fgrep -q "$gfxline" "$grubfile" || {
   mv "$tmpgrubfile" "$grubfile"
   update-grub
 }
+
+# Setup the nodm config to login the user automatically
+
+nodmfile="/etc/default/nodm"
+tmpnodmfile="/tmp/nodmtmp.$$"
+sed -e " s,NODM_ENABLED=.*,NODM_ENABLED=true,; s,NODM_USER=.*,NODM_USER=$vagrantuser,; " < "$nodmfile" > "$tmpnodmfile"
+mv "$tmpnodmfile" "$nodmfile"
+
+# Setup the openbox menu to run Proof General
+
+obfile="/etc/xdg/openbox/menu.xml"
+tmpobfile="/tmp/obmenutmp.$$"
+sed -e ' s,\(<menu id=./Debian. />\).*,\1 <item label="Proof General" icon="/usr/share/pixmaps/proofgeneral.xpm"><action name="Execute"><execute>proofgeneral</execute></action></item>, ' < "$obfile" > "$tmpobfile"
+mv "$tmpobfile" "$obfile"
+
+# Need to restart for login changes to take effect
+reboot # This will cause an error in Vagrant. Ignore that.
--- init_as_user.sh
+++ init_as_user.sh
@@ -2,11 +2,13 @@
 
 cd $HOME
 
-workdir="sf"
-
-test -d "$workdir" || mkdir -p "$workdir"
-cd "$workdir"
-
+# Download and unpack the "software foundations" book
 test -s "sf.tar.gz" || wget "https://www.cis.upenn.edu/~bcpierce/sf/current/sf.tar.gz"
 tar zxf "sf.tar.gz"
+
+# Download Chlipala's book "Certified Programming with Dependent Types" and its code
+test -s "cpdt.pdf" || wget "http://adam.chlipala.net/cpdt/cpdt.pdf"
+test -s "cpdt.tgz" || wget "http://adam.chlipala.net/cpdt/cpdt.tgz"
+tar zxf "cpdt.tgz"
+
 
